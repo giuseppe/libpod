9002dce687ee7f86de447ce7ba214f88cc275c24
Paul Holzinger <pholzing@redhat.com>
2026-09-07 17:11:37 +0200

    pkg/machine/e2e: use withFakeImage for excessive cpus test
    The test should fail before booting a real VM so we can use the fake
    image which should make the test a few seconds faster.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- pkg/machine/e2e/set_test.go
+++ pkg/machine/e2e/set_test.go
@@ -16,7 +16,7 @@ var _ = Describe("podman machine set", func() {
 		skipIfWSL("WSL cannot change cpus via set")
 		name := randomString()
 		i := new(initMachine)
-		session, err := mb.setName(name).setCmd(i.withImage(mb.imagePath)).run()
+		session, err := mb.setName(name).setCmd(i.withFakeImage(mb)).run()
 		Expect(err).ToNot(HaveOccurred())
 		Expect(session).To(Exit(0))
 
